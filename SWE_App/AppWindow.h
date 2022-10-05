b69469702911d25767e54f247ebeebefa3227691
Connor Maher <therealconnor.maher@gmail.com>
2022-10-05 16:09:38 -0600

    Created all buttons in header file

--- SWE_App/AppWindow.h
+++ SWE_App/AppWindow.h
@@ -4,6 +4,32 @@
 
 class AppWindow : public wxFrame
 {
+	wxButton* Zero = nullptr;
+	wxButton* One = nullptr;
+	wxButton* Two = nullptr;
+	wxButton* Three = nullptr;
+	wxButton* Four = nullptr;
+	wxButton* Five = nullptr;
+	wxButton* Six = nullptr;
+	wxButton* Seven = nullptr;
+	wxButton* Eight = nullptr;
+	wxButton* Nine = nullptr;
+
+	wxButton* Add = nullptr;
+	wxButton* Subtract = nullptr;
+	wxButton* Multiply = nullptr;
+	wxButton* Divide = nullptr;
+	wxButton* Equal = nullptr;
+	wxButton* Clear = nullptr;
+
+	wxButton* Lparetheses = nullptr;
+	wxButton* Rparetheses = nullptr;
+	wxButton* Mod = nullptr;
+	wxButton* Sin = nullptr;
+	wxButton* Cos = nullptr;
+	wxButton* Tan = nullptr;
+
+	wxTextCtrl* Textbox = nullptr;
 
 public:
 	AppWindow();
