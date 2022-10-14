ceab020722759da4d656e9236ae0c25720311b51
Connor Maher <therealconnor.maher@gmail.com>
2022-10-13 18:30:01 -0600

    Created all methods for buttons, just need to implment the function for each of them now

--- SWE_App/ButtonFactory.cpp
+++ SWE_App/ButtonFactory.cpp
@@ -14,3 +14,113 @@ wxButton* ButtonFactory::CreateZeroButton(wxWindow *window)
 	wxButton* Zero = new wxButton(window, 0, "0", wxPoint(100, 350), wxSize(50, 50));
 	return Zero;
 }
+
+wxButton* ButtonFactory::CreateOneButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateTwoButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateThreeButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateFourButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateFiveButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateSixButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateSevenButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateEightButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateNineButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateADDButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateSUBButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateMULTButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateDIVIButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateEQUALButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateCLEARButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreatePLUSandMINButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateLPAREButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateRPAREButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateMODButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateSINButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateCOSButton(wxWindow* window)
+{
+	return nullptr;
+}
+
+wxButton* ButtonFactory::CreateTANButton(wxWindow* window)
+{
+	return nullptr;
+}
--- SWE_App/ButtonFactory.h
+++ SWE_App/ButtonFactory.h
@@ -7,31 +7,30 @@ class ButtonFactory
 public:
 
 	static wxButton* CreateZeroButton(wxWindow *window);
-	static wxButton* CreateOneButton();
-	static wxButton* CreateTwoButton();
-	static wxButton* CreateThreeButton();
-	static wxButton* CreateFourButton();
-	static wxButton* CreateFiveButton();
-	static wxButton* CreateSixButton();
-	static wxButton* CreateSevenButton();
-	static wxButton* CreateEightButton();
-	static wxButton* CreateNineButton();
+	static wxButton* CreateOneButton(wxWindow *window);
+	static wxButton* CreateTwoButton(wxWindow* window);
+	static wxButton* CreateThreeButton(wxWindow* window);
+	static wxButton* CreateFourButton(wxWindow* window);
+	static wxButton* CreateFiveButton(wxWindow* window);
+	static wxButton* CreateSixButton(wxWindow* window);
+	static wxButton* CreateSevenButton(wxWindow* window);
+	static wxButton* CreateEightButton(wxWindow* window);
+	static wxButton* CreateNineButton(wxWindow* window);
 
-	static wxButton* CreateADDButton();
-	static wxButton* CreateSUBButton();
-	static wxButton* CreateMULTButton();
-	static wxButton* CreateDIVIButton();
-	static wxButton* CreateEQUALButton();
-	static wxButton* CreateCLEARButton();
-	static wxButton* CreatePLUSandMINButton();
+	static wxButton* CreateADDButton(wxWindow* window);
+	static wxButton* CreateSUBButton(wxWindow* window);
+	static wxButton* CreateMULTButton(wxWindow* window);
+	static wxButton* CreateDIVIButton(wxWindow* window);
+	static wxButton* CreateEQUALButton(wxWindow* window);
+	static wxButton* CreateCLEARButton(wxWindow* window);
+	static wxButton* CreatePLUSandMINButton(wxWindow* window);
 
-	static wxButton* CreateLPAREButton();
-	static wxButton* CreateRPAREButton();
-	static wxButton* CreateMODButton();
-	static wxButton* CreateSINButton();
-	static wxButton* CreateCOSButton();
-	static wxButton* CreateTANButton();
-
+	static wxButton* CreateLPAREButton(wxWindow* window);
+	static wxButton* CreateRPAREButton(wxWindow* window);
+	static wxButton* CreateMODButton(wxWindow* window);
+	static wxButton* CreateSINButton(wxWindow* window);
+	static wxButton* CreateCOSButton(wxWindow* window);
+	static wxButton* CreateTANButton(wxWindow* window);
 
 	ButtonFactory(wxWindow *window);
 
