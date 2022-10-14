ee3724baf6cdf43d527b1c339449e2739d0119db
Connor Maher <therealconnor.maher@gmail.com>
2022-10-13 19:09:20 -0600

    Finished button factory

--- SWE_App/AppWindow.cpp
+++ SWE_App/AppWindow.cpp
@@ -28,30 +28,30 @@ AppWindow::AppWindow() :
 	 wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(400, 200), wxSize(500,500) )
 {
 	 Zero = ButtonFactory::CreateZeroButton(this);
-	 One = new wxButton(this, 1,"1", wxPoint(25,275), wxSize(50,50));
-	 Two = new wxButton(this, 2,"2", wxPoint(100,275), wxSize(50,50));
-	 Three = new wxButton(this, 3,"3", wxPoint(175,275), wxSize(50,50));
-	 Four = new wxButton(this, 4,"4", wxPoint(25,200), wxSize(50,50));
-	 Five = new wxButton(this, 5,"5", wxPoint(100,200), wxSize(50,50));
-	 Six = new wxButton(this, 6,"6", wxPoint(175,200), wxSize(50,50));
-	 Seven = new wxButton(this, 7,"7", wxPoint(25,125), wxSize(50,50));
-	 Eight = new wxButton(this, 8,"8", wxPoint(100,125), wxSize(50,50));
-	 Nine = new wxButton(this, 9,"9", wxPoint(175,125), wxSize(50,50));
+	 One = ButtonFactory::CreateOneButton(this);
+	 Two = ButtonFactory::CreateTwoButton(this);
+	 Three = ButtonFactory::CreateThreeButton(this);
+	 Four = ButtonFactory::CreateFourButton(this);
+	 Five = ButtonFactory::CreateFiveButton(this);
+	 Six = ButtonFactory::CreateSixButton(this);
+	 Seven = ButtonFactory::CreateSevenButton(this);
+	 Eight = ButtonFactory::CreateEightButton(this);
+	 Nine = ButtonFactory::CreateNineButton(this);
 
-	 Add = new wxButton(this, 101,"+", wxPoint(275,125), wxSize(50,50));
-	 Subtract = new wxButton(this, 102,"-", wxPoint(350,125), wxSize(50,50));
-	 Multiply = new wxButton(this, 103,"*", wxPoint(275,200), wxSize(50,50));
-	 Divide = new wxButton(this, 104,"/", wxPoint(350,200), wxSize(50,50));
-	 Equal = new wxButton(this, 105,"=", wxPoint(175,350), wxSize(50,50));
-	 Clear = new wxButton(this, 106,"C", wxPoint(25,350), wxSize(50,50));
-	 PlusAndMin = new wxButton(this, 106, "+/-", wxPoint(275, 350), wxSize(50, 50));
+	 Add = ButtonFactory::CreateADDButton(this);
+	 Subtract = ButtonFactory::CreateSUBButton(this);
+	 Multiply = ButtonFactory::CreateMULTButton(this);
+	 Divide = ButtonFactory::CreateDIVIButton(this);
+	 Equal = ButtonFactory::CreateEQUALButton(this);
+	 Clear = ButtonFactory::CreateCLEARButton(this);
+	 PlusAndMin = ButtonFactory::CreatePLUSandMINButton(this);
 
-	 Lparetheses = new wxButton(this, 201,"(", wxPoint(275,275), wxSize(50,50));
-	 Rparetheses = new wxButton(this, 202,")", wxPoint(350,275), wxSize(50,50));
-	 Mod = new wxButton(this, 203,"Mod", wxPoint(425,125), wxSize(50,50));
-	 Sin = new wxButton(this, 204,"Sin", wxPoint(425,200), wxSize(50,50));
-	 Cos = new wxButton(this, 205,"Cos", wxPoint(425,275), wxSize(50,50));
-	 Tan = new wxButton(this, 206,"Tan", wxPoint(425,350), wxSize(50,50));
+	 Lparetheses = ButtonFactory::CreateLPAREButton(this);
+	 Rparetheses = ButtonFactory::CreateRPAREButton(this);
+	 Mod = ButtonFactory::CreateMODButton(this);
+	 Sin = ButtonFactory::CreateSINButton(this);
+	 Cos = ButtonFactory::CreateCOSButton(this);
+	 Tan = ButtonFactory::CreateTANButton(this);
 
 	 Textbox = new wxTextCtrl(this, 300, "", wxPoint(25, 10), wxSize(450, 100));
 
--- SWE_App/ButtonFactory.cpp
+++ SWE_App/ButtonFactory.cpp
@@ -36,73 +36,73 @@ wxButton* ButtonFactory::CreateThreeButton(wxWindow *window)
 wxButton* ButtonFactory::CreateFourButton(wxWindow *window)
 {
 	wxButton* Four = new wxButton(window, 4, "4", wxPoint(25, 200), wxSize(50, 50));
-	return nullptr;
+	return Four;
 }
 
 wxButton* ButtonFactory::CreateFiveButton(wxWindow *window)
 {
 	wxButton* Five = new wxButton(window, 5, "5", wxPoint(100, 200), wxSize(50, 50));
-	return nullptr;
+	return Five;
 }
 
 wxButton* ButtonFactory::CreateSixButton(wxWindow *window)
 {
 	wxButton* Six = new wxButton(window, 6, "6", wxPoint(175, 200), wxSize(50, 50));
-	return nullptr;
+	return Six;
 }
 
 wxButton* ButtonFactory::CreateSevenButton(wxWindow *window)
 {
 	wxButton* Seven = new wxButton(window, 7, "7", wxPoint(25, 125), wxSize(50, 50));
-	return nullptr;
+	return Seven;
 }
 
 wxButton* ButtonFactory::CreateEightButton(wxWindow *window)
 {
 	wxButton* Eight = new wxButton(window, 8, "8", wxPoint(100, 125), wxSize(50, 50));
-	return nullptr;
+	return Eight;
 }
 
 wxButton* ButtonFactory::CreateNineButton(wxWindow *window)
 {
 	wxButton* Nine = new wxButton(window, 9, "9", wxPoint(175, 125), wxSize(50, 50));
-	return nullptr;
+	return Nine;
 }
 
 wxButton* ButtonFactory::CreateADDButton(wxWindow *window)
 {
 	wxButton* Add = new wxButton(window, 101, "+", wxPoint(275, 125), wxSize(50, 50));
-	return nullptr;
+	return Add;
 }
 
 wxButton* ButtonFactory::CreateSUBButton(wxWindow *window)
 {
 	wxButton* Subtract = new wxButton(window, 102, "-", wxPoint(350, 125), wxSize(50, 50));
-	return nullptr;
+	return Subtract;
 }
 
 wxButton* ButtonFactory::CreateMULTButton(wxWindow *window)
 {
 	wxButton* Multiply = new wxButton(window, 103, "*", wxPoint(275, 200), wxSize(50, 50));
-	return nullptr;
+	return Multiply;
 }
 
 wxButton* ButtonFactory::CreateDIVIButton(wxWindow *window)
 {
 	wxButton* Divide = new wxButton(window, 104, "/", wxPoint(350, 200), wxSize(50, 50));
-	return nullptr;
+	return Divide;
 }
 
 wxButton* ButtonFactory::CreateEQUALButton(wxWindow *window)
 {
 	wxButton* Equal = new wxButton(window, 105, "=", wxPoint(175, 350), wxSize(50, 50));
-	return nullptr;
+	return Equal;
 }
 
 wxButton* ButtonFactory::CreateCLEARButton(wxWindow *window)
 {
 	wxButton* Clear = new wxButton(window, 106, "C", wxPoint(25, 350), wxSize(50, 50));
-	return nullptr;
+	return Clear;
 }
 
 wxButton* ButtonFactory::CreatePLUSandMINButton(wxWindow *window)
